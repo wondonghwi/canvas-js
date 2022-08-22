daf56f69549178501d7cb199b465677a2dacbedc
wondonghwi <ehdehd1289@naver.com>
2022-08-22 11:13:34 +0900

    ✨ add canvas fill, draw, erase, colorChange

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
+
+const modeBtn = document.querySelector('#mode-btn');
+const destroyBtn = document.querySelector('#destroy-btn');
+const eraserBtn = document.querySelector('#eraser-btn');
+const colorOption = Array.from(document.querySelectorAll('.color-option'));
+const color = document.querySelector('#color');
+const lineWidth = document.querySelector('#line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 600;
-canvas.height = 600;
-ctx.lineWidth = 2;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
 
 const onMove = e => {
   if (isPainting) {
@@ -15,14 +25,65 @@ const onMove = e => {
   ctx.moveTo(e.offsetX, e.offsetY);
 };
 
-const startPainting = () => {
-  isPainting = true;
-};
+const startPainting = () => (isPainting = true);
+
 const cancelPainting = () => {
   isPainting = false;
+  ctx.beginPath();
 };
+
+const onLineWidthChange = e => (ctx.lineWidth = e.target.value);
+
+const onColorChange = e => {
+  ctx.strokeStyle = e.target.value;
+  ctx.fillStyle = e.target.value;
+};
+
+const onColorClick = e => {
+  const colorValue = e.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+};
+
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = 'Fill';
+  } else {
+    isFilling = true;
+    modeBtn.innerText = 'Draw';
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = 'white';
+  isFilling = false;
+  modeBtn.innerText = 'Fill';
+}
 
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
+canvas.addEventListener('click', onCanvasClick);
+
+lineWidth.addEventListener('change', onLineWidthChange);
+
+color.addEventListener('change', onColorChange);
+
+colorOption.forEach(color => color.addEventListener('click', onColorClick));
+modeBtn.addEventListener('click', onModeClick);
+destroyBtn.addEventListener('click', onDestroyClick);
+eraserBtn.addEventListener('click', onEraserClick);
